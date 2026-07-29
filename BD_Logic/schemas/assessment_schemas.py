afcf101bbbacf17ef15dce68bc59ac422b7cee84
yashwithareddy <yashwithakandula@gmail.com>
2026-07-29 20:21:37 +0530

    D2- 1.updated the schemas and created response file

--- BD_Logic/schemas/assessment_schemas.py
+++ BD_Logic/schemas/assessment_schemas.py
@@ -3,5 +3,27 @@ from pydantic import BaseModel
 class AssessmentRequest(BaseModel):
     
     session_id: str
-    
+    user_id: int
+
+    lesson_id: int
+
     expected_sign: str
+
+    predicted_sign: str
+
+    confidence: float
+
+    hand_shape: float
+
+    finger_position: float
+
+    motion: float
+
+    timing: float
+    
+ class AssessmentResponse(BaseModel):
+    
+    accuracy: float
+    result: str
+    
+       
