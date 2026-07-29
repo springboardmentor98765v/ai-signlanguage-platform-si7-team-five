f6aae8a539cb51f9a3e37fb2111ddc4e2afd23f2
yashwithareddy <yashwithakandula@gmail.com>
2026-07-29 19:51:17 +0530

    d1- 8. recommendation engine created

---
+++ BD_Logic/recommendation/recommendation_engine.py
@@ -0,0 +1,10 @@
+class RecommendationEngine:
+    def recommend(self,weak_letters) 
+       recommendations = []
+    
+       for letter in weak_letters:
+        recommendations.append(
+            f"Practice letter {letter} to improve your accuracy."
+        )
+    
+       return recommendations
