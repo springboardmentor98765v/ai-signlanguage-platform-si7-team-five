ad63fdbf1917cea6994976f2c3e4c088387a686d
yashwithareddy <yashwithakandula@gmail.com>
2026-07-29 19:31:46 +0530

    D1- 2. defined the constants

---
+++ BD_Logic/utils/constants.py
@@ -0,0 +1,17 @@
+SCORING_PARAMETERS = {
+    "hand_shape":30,
+
+    "finger_position":25,
+
+    "motion":20,
+
+    "timing":15,
+
+    "confidence":10
+}
+
+CERTIFICATE_MIN_SCORE = 85
+
+WEAK_SIGN_THRESHOLD = 60
+
+RECOMMENDATION_COUNT = 3
